e9ce0eadc75ab2f6e73a4f48e1ef07aadf2d45e5
patel4ay <60115893+patel4ay@users.noreply.github.com>
2020-02-15 15:50:23 -0500

    Update project-1.ps1

--- powershell/project-1.ps1
+++ powershell/project-1.ps1
@@ -1,2 +1,2 @@
-#This script extracts lines from a text file with the word we want in the line.  
+#This script extracts lines from a text file with the word we want in the line. It shows all the lines that has the same word. 
 Get-Content C:\txt.txt | Where-Object {$_.Contains( 'word')}
