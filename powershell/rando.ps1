4b3a9879d1ae77268be38d695efa5ec8b1a9c75c
patel4ay <patel4ay@mail.uc.edu>
2020-03-10 20:32:30 -0400

    comment

---
+++ powershell/rando.ps1
@@ -0,0 +1,27 @@
+﻿#$RANDO=0
+#for($i=0; $i -lt 5; $i++){
+ #   $RANDO=Get-Random -Maximum 1000 -Minimum 1
+ #   Write-Host($RANDO)
+ #   Add-Content $LogFile "INFO: Random number is ${RANDO}"
+#}
+#$Machines='localhost'
+#Foreach($machine in $Machines)
+#{
+#$RCounters=Get-Counter -ListSet * -ComputerName $Machines "There are {0} counters on {1}" -f $RCounters.count, ($machine)
+#} 
+#$Machines='localhost'
+#Foreach($machine in $Machines)
+#{
+#$pt = (Get-Counter -Counter "\Processor(_Total)\% Processor Time").CounterSamples.CookedValue
+#$pt
+#}
+$Machines = 'localhost'
+foreach($machine in $Machines)
+{
+$pt = (Get-Counter -Counter "\Processor(_Total)\% Processor Time" -SampleInterval 1 -MaxSamples 3).CounterSamples.CookedValue
+ $sample=1
+ foreach ($p in $pt) {$date=Get-Date -Format g
+     "{3} - Sample {2}: CPU is at {0}% on {1}" -f [int]$p,$machine, $sample, $date | out-file C:\output.txt
+        $sample++
+        }
+}
--- powershell/sysinfo.ps1
+++ powershell/sysinfo.ps1
@@ -1,1 +1,20 @@
-Get-Content C:\txt.txt | Where-Object {$_.Contains('Donkey')}
+#This is a script to send an email with info about our server
+
+#a function that returns an IP address starting with 172
+function getIP {
+    (get-netipaddress).IPAddress | Select-String "192*"
+}
+
+#set the IP variable
+$IP=getIP
+$HostName=Get-Host | Select-Object Name
+$Version=Get-Host | Select-Object Version
+#set the Body variable
+$BODY = "This machine's IP is $IP. User is $env:USERNAME. Hostname is $Hostname. PowerShell Version $Version. Today's Date is $Get-Date"
+Write-Host($BODY)
+
+#build and send the email 
+#Send-MailMessage -To "patel4ay@mail.uc.edu" -From "me@mail.com" -Subject "IT3038C Windows SysInfo" -Body $BODY -SmtpServer smtp.gmail.com -Port 587 -UseSsl -Credential (Get-Credential)
+
+#confirm we reached the end
+Write-Host("Email sent") 
